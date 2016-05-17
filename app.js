1729ebe70849a21be554dbc53c80512b6ab3fea2
skozlov <sergey.kozlovich@landsend.com>
2016-05-17 18:11:06 +0300

    Default heroku port

--- app.js
+++ app.js
@@ -13,5 +13,5 @@ app.use(express.static(__dirname + '/public'));
 //app.use(app.router);
 router(app);
 
-app.listen(8080);
+app.listen(process.env.PORT || 8080);
 console.log("App listening on port 8080");
